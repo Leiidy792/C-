9aa509cfa977ccb05a88bdd39fc64d261a17e55f
Leiidy792 <85407543+Leiidy792@users.noreply.github.com>
2023-01-26 22:26:14 -0400

    Add files via upload

---
+++ Program 7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_7
+{/// <summary>
+ /// 12. Leer 10 números enteros, almacenarlos  y determinar a cuánto es igual el promedio entero de los datos.
+
+/// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arreglo = new int[10] { 34, 54, 8, 0, 36, 16, 28, 19, 2, 3 };
+            int sumaTotal = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                sumaTotal = sumaTotal + arreglo[i];
+            }
+
+            int promedio = sumaTotal / 10;
+
+            Console.WriteLine($"El promedio de los elementos es {promedio}");
+        }
+    }
+    }
